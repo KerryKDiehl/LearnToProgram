af3baa2f6c1769307e4fa33414e4d0d781038aaa
KerryKDiehl <kerrykdiehl@gmail.com>
2014-10-26 21:10:12 -0700

    Old-school Roman Numerals program first run

---
+++ 9.rb
@@ -0,0 +1,119 @@
+=begin
+return_val = puts 'This puts returned:'
+puts return_val
+
+def say_moo number_of_moos
+	puts 'moooooooo... '*number_of_moos
+	return 'We all live in'
+	'yellow submarine'
+end
+
+x = say_moo 3
+puts x.capitalize + ', dude....'
+puts x + '.'
+=end
+
+# Improved ask method.  Try cleaning up the ask method by removing the answer variable.
+=begin
+def ask question
+	while true
+		puts question
+		reply = gets.chomp.downcase
+
+		if (reply == 'yes' || reply == 'no')
+			if reply == 'yes'
+				return true
+			else 
+				return false
+			end
+			break
+		else
+			puts 'Please answer "yes" or "no".'
+		end
+	end
+end
+
+puts 'Hello, and thank you for participating in our study...'
+puts
+
+ask 'Do you like eating tacos'
+ask 'Do you like eating burritos?'
+wets_bed = ask 'Do you wet the bed?'
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horachata?'
+ask 'Do you like eating flautas?'
+
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for participating'
+puts
+puts wets_bed
+=end
+
+
+
+# Old-school Roman numerals.  Write a method that when passed an integer between 
+#1 and 3000 returns a string containing the proper old-school Roman numerals.
+puts 'Please enter a number'
+number = gets.chomp.to_i
+def roman_numeral number
+	if number/1000 > 0
+		m = number/1000		
+		uH = number%1000
+	else
+		m = 0
+		uH = number%1000
+	end
+	if uH/500 > 0
+		d = uH/500
+		h = uH%500
+	else
+		d = 0
+		h = uH%500
+	end
+	if h/100 > 0
+		c = h/100
+		uT = h%100
+	else
+		c = 0
+		uT = h%100
+	end				
+	if uT/50 > 0
+		l = uT/50
+		t = uT%50
+	else
+		l = 0
+		t = uT%50
+	end
+	if t/10 > 0
+		x = t/10
+		uS = t%10
+	else
+		x = 0
+		uS = t%10
+	end	
+	if uS/5 > 0
+		v = uS/5
+		s = uS%5
+	else
+		v = 0
+		s = uS%5
+	end
+	if s/1 > 0
+		i = s/1
+	else
+		i = 0
+	end
+	puts 'M'*m.to_i + 'D'*d.to_i + 'C'*c.to_i + 'L'*l.to_i + 'X'*x.to_i + 'V'*v.to_i + 'I'*i.to_i
+end
+roman_numeral number
+
+
+
+
+
+
+
+
