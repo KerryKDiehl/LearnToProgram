c53cbf6889cfe2c7d03934e62850d4c0bc773d9f
KerryKDiehl <kerrykdiehl@gmail.com>
2014-10-27 16:46:44 -0700

    Ch 9, question 3

--- 9.rb
+++ 9.rb
@@ -75,18 +75,58 @@ def roman_numeral_shortened number
 		singles = upperSingles%5
 	i = singles/1
 
-	numeral_multiple = [m,d,c,l,x,v,i]
-	place = 0
-	if numeral_multiple[place] == nil 
-		numeral_multiple[place] = 0
-		place += 1
-	end
-	numeral_multiple
-
+	puts "In the early days of Roman numerals, the number you choose would have been written as follows:"
 	puts 'M'*m + 'D'*d + 'C'*c + 'L'*l + 'X'*x + 'V'*v + 'I'*i
 end
 roman_numeral_shortened number
 
+
+
+
+
+# Modern Roman numerals.  Write a method that when passed an integer between 
+#1 and 3000 returns a string containing the proper "modern" Roman numerals.
+puts 'Please enter a number'
+number = gets.chomp.to_i
+def roman_numeral_shortened number
+	
+	m = number/1000		
+		upperHundreds = number%1000
+	d = upperHundreds/500
+		hundreds = upperHundreds%500
+	c = hundreds/100
+		upperTens = hundreds%100	
+	l = upperTens/50
+		tens = upperTens%50	
+	x = tens/10
+		upperSingles = tens%10
+	v = upperSingles/5
+		singles = upperSingles%5
+	i = singles/1
+
+	cM, cD, xC, xL, iX, iV = 0,0,0,0,0,0
+
+	numeral_multiple = [m,cM,d,cD,c,xC,l,xL,x,iX,v,iV,i]
+
+	smNum = 4
+	while smNum < numeral_multiple.length
+		if numeral_multiple[smNum] == 4
+			if numeral_multiple[smNum - 2] == 1
+				numeral_multiple[smNum - 2] = 0
+				numeral_multiple[smNum - 3] = 1
+			else
+				numeral_multiple[smNum - 1] = 1
+			end
+			numeral_multiple[smNum] = 0
+		end
+		smNum += 4
+	end
+	numeral_multiple
+
+	puts 'In "present day" Roman numerals, the number you choose would have been written as follows:'
+	puts 'M'*numeral_multiple[0] + 'CM'*numeral_multiple[1] + 'D'*numeral_multiple[2] + 'CD'*numeral_multiple[3] + 'C'*numeral_multiple[4] + 'XC'*numeral_multiple[5] + 'L'*numeral_multiple[6] + 'XL'*numeral_multiple[7] + 'X'*numeral_multiple[8] + 'IX'*numeral_multiple[9] + 'V'*numeral_multiple[10] + 'IV'*numeral_multiple[11] + 'I'*numeral_multiple[12]
+end
+roman_numeral_shortened number
 
 
 
